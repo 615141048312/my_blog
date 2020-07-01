2f19ebda37ca144264d540fef3164357490ef392
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-07-01 16:28:25 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '31e704da6455c0dcd9a2b9f53c79961daa65ab0bca057ea9045efddcf416c41921641e8f4e98a6e66a4ea34c061de90ee0fcb74736f9b3a228b1b607997bd0c5'
